c4a89bfc3aeb8aa9b0203a7111deb8b32f05d1e6
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-07 22:58:48 +0300

    props imroved

--- src/2-Props/Props.js
+++ src/2-Props/Props.js
@@ -5,11 +5,29 @@ const Props = () => {
     // let variable = "Mustang"
     // const myElement2 = <Car brand={variable}/>
 
-    const carInfo = {
-        name : "Ford",
-        model: "Mustang"
-    }
+    // const carInfo = {
+    //     name : "Ford",
+    //     model: "Mustang"
+    // }
     const myElement3 = <Car brand={carInfo}/>
+
+    const person = {
+        name :"Hakan",
+        age :53,
+        location : "Turkey"
+    }
+    const myCar = {
+        carName : "Ford",
+        carAge :5,
+        model :"Mustang"
+    }
+
+    const myProps= {
+        person, myCar
+    }
+
+    const  myElement4 = <Car definition={myProps}/>
+
     return (
     <>
     {/* {myElement}
