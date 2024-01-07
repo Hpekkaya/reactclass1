75ad2420f9441edc504b671dd99124f6b53bd8a0
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-07 14:51:11 +0300

    styles tried

--- src/1-JSX/JSX.js
+++ src/1-JSX/JSX.js
@@ -2,8 +2,15 @@
 
 
 const JSX=()=> {
+    const name ="hakan"
     return(
-        <div>This JSX component</div>
+        <div className="App">
+            {/* This is JSX component */}
+            <h1 className="hakan">Hello {name}</h1>
+            <h1>{10+10}</h1>
+            <h1>{name.toUpperCase()}</h1>
+            <h2 style={{color:"red"}}>Ahmet</h2>
+        </div>
     )
 }
 
--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 
-
 import JSX from "./1-JSX/JSX"
+import './index.css';
+
 
 function App() {
-  return (
-   <div>
-      {/* <p>This is the First React Application </p> */}
-      <JSX/>
-   </div>
-  );
-}
-
-export default App;
+    return (
+       <div>
+          {/* <p>This is the First React Application </p> */}
+          <JSX/>
+       </div>
+      );
+    }
+    
+    export default App;
 
 
