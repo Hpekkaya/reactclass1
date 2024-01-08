5413a42bb93589648b4d1da18cc33ed5c853ddf4
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-08 09:38:06 +0300

    events repeated

--- src/3-Events/Events.js
+++ src/3-Events/Events.js
@@ -5,9 +5,22 @@ const Events = () => {
     const shoot = ()=> {
         alert("Great Shot !")
     }
+    const shoot2 = (a)=> {
+        alert(a)
+    }
+    const shoot3 = (a,b)=> {
+        alert(b.type)
+        b.target.value = "Value is changed"
+
+        console.log(b.target)
+        console.log(b.target.value)
+        console.log(b)   //In order to see what type of base events 
+    }
   return (
     <div>
         <button onClick={shoot}>Take the shoot</button>
+        <button onClick={()=>shoot2("Goal !")}>Take the shoot2</button>
+        <button onClick={(event)=>shoot3("Goal !",event)}>Take the shoot3</button>
     </div>
   )
 }
