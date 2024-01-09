2e14aa8a962add72b2b8ab5414e14f15f8c1f6cf
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-09 22:37:20 +0300

    State applied

--- src/6-UseStateHook/UseStateHook.js
+++ src/6-UseStateHook/UseStateHook.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseStateHook = () => {
+
+    const [color, setColor] = useState("brown")
+
   return (
-    <div>UseStateHook</div>
+    <div>
+        <h1>My favourite color is {color}</h1>
+        <button type="button" onClick={()=>setColor("blue")}>Change Color</button>
+    </div>
   )
 }
 
