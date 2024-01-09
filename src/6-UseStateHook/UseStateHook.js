14a01bed4bf074c999292212a9b9a2ec3c92b536
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-09 23:01:11 +0300

    update applied

--- src/6-UseStateHook/UseStateHook.js
+++ src/6-UseStateHook/UseStateHook.js
@@ -11,12 +11,19 @@ const UseStateHook = () => {
         color:"Red"
     })
 
+    const updateColor = ()=> {
+        setCar(previousState => {
+            return {...previousState, color:"blue"}
+        })
+    }
+
   return (
     <div>
         <h1>My favourite color is {color}</h1>
         <button type="button" onClick={()=>setColor("blue")}>Change Color</button>
         <h1>It is a  {car.color} {car.brand} {car.model} from {car.year}</h1>
         <button type='button' onClick={()=> setCar({...car, brand:"BMW", year :"2000"})}>Change Car Year</button>
+        <button type='button' onClick={updateColor}>Update Car Color</button>
     </div>
   )
 }
