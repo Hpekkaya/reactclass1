3d43ed907bba0db534e976b12710c8776331fd40
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-11 10:17:20 +0300

    Forms created

---
+++ src/8-Forms/Forms.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Forms = () => {
+  const [name,setName]
+  return (
+    <div>
+      <form>
+        <label> Enter your name :
+          <input></input>
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default Forms
