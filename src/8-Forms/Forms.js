381df811ebc078db803b4028d8b2c4c54bac16ad
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-11 10:58:26 +0300

    Handlechange

--- src/8-Forms/Forms.js
+++ src/8-Forms/Forms.js
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Forms = () => {
-  const [name,setName]
+  const [name, setName] = React.useState("Enter Name");
+
+  const [inputs, setInputs] = React.useState({});
+
+  // const handleSubmit =(event)=>{
+  //   event.preventDefault();
+  //   alert(`The name you entered was : ${name}`)
+  // }
+    const handleChange = (event)=>{
+      const name = event.target.name
+      const value = event.target.value
+      setInputs( values => ({...values, [name]:value}))
+
+    }
+    const handleSubmit2 = (event)=> {
+      event.preventDefault();
+      console.log(inputs)
+    }
   return (
     <div>
-      <form>
-        <label> Enter your name :
-          <input></input>
-        </label>
+      {/* <form onSubmit={handleSubmit}>
+        <label> Enter your name : 
+          <input 
+          type="text" 
+          value={name}
+          onChange={(event)=>setName(event.target.value)}
+          />
+        </label> <br></br>
+        <input type='submit'/>
+      </form> */}
+      <form onSubmit={handleSubmit2}>
+        <label> Enter your name : </label>
+        <input
+          type="text"
+          name="username"
+          value={inputs.username || ""}
+          onChange={handleChange}
+        />{" "}
+        <br></br>
+        <label> Enter your Age   : </label>
+        <input
+          type="number"
+          name="age"
+          value={inputs.age || ""}
+          onChange={handleChange}
+        />
+        <input type="submit"/>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Forms
+export default Forms;
