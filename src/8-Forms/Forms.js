ed29daf98f17e97c67b86eeaeca23cf0bf2555b2
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-11 11:58:20 +0300

    Formik created

--- src/8-Forms/Forms.js
+++ src/8-Forms/Forms.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
+import MyFormik from "./FormikYup/MyFormik";
 
 const Forms = () => {
-  const [name, setName] = React.useState("Enter Name");
+  // const [name, setName] = React.useState("Enter Name");
   
-  const [inputs, setInputs] = React.useState({});
+  // const [inputs, setInputs] = React.useState({});
   
-  const [myCar, setmyCar] = React.useState("Volvo");
+  // const [myCar, setmyCar] = React.useState("Volvo");
   // const handleSubmit =(event)=>{
   //   event.preventDefault();
   //   alert(`The name you entered was : ${name}`)
   // }
-    const handleChange = (event)=>{
-      const name = event.target.name
-      const value = event.target.value
-      setInputs( values => ({...values, [name]:value}))
+    // const handleChange = (event)=>{
+    //   const name = event.target.name
+    //   const value = event.target.value
+    //   setInputs( values => ({...values, [name]:value}))
 
-    }
-    const handleSubmit2 = (event)=> {
-      event.preventDefault();
-      console.log(inputs)
-    }
-    const handleChange2 = (event)=>{
-            setmyCar(event.target.value)
-    }
+    // }
+    // const handleSubmit2 = (event)=> {
+    //   event.preventDefault();
+    //   console.log(inputs)
+    // }
+    // const handleChange2 = (event)=>{
+    //         setmyCar(event.target.value)
+    // }
   return (
     <div>
       {/* <form onSubmit={handleSubmit}>
@@ -35,7 +36,7 @@ const Forms = () => {
         </label> <br></br>
         <input type='submit'/>
       </form> */}
-      <form onSubmit={handleSubmit2}>
+      {/* <form onSubmit={handleSubmit2}>
         <label> Enter your name : </label>
         <input
           type="text"
@@ -59,7 +60,8 @@ const Forms = () => {
           <option value="Volvo">Volvo</option>
           <option value="Honda">Honda</option>
         </select>
-      </form>
+      </form> */}
+      <MyFormik/>
     </div>
   );
 };
