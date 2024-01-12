5c4ce7102c31e197bc198a84ca0c5568afde1c29
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-12 12:55:20 +0300

    Constructive method tried

--- src/2-Props/Car1.js
+++ src/2-Props/Car1.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
-const Car1 = (props) => {
+const Car1 = ({definition,addition}) => {
   return (
     <div>
         {/* <h2>My Car brand is {props.brand} and model is {props.model}</h2> */}
         {/* <h2>My Car :  </h2>
         <h2> Brand : {props.brand.name} Model : {props.brand.model}  </h2>
         <h2> Color : {props.brand.color} Type : {props.brand.type}  </h2> */}
-        
-        <h2>{props.definition.person.name}</h2>
+
+        {/* (props) */}
+        {/* <h2>{props.definition.person.name}</h2>
         <h2>{props.definition.person.age}</h2>
         <h2>{props.definition.myCar.carName}</h2>
-        <h2>{props.definition.myCar.carAge}</h2>
+        <h2>{props.definition.myCar.carAge}</h2> */}
+
+        {/* ({definition,addition}) */}
+        <h2>{definition.myCar.carName}</h2>
+        <h2>{addition.carModel}</h2>
+        
     </div>
   )
 }
--- src/2-Props/Props1.js
+++ src/2-Props/Props1.js
@@ -6,18 +6,36 @@ const Props1 = () => {
     // let var1 = "Tesla", var2 = "Electronics"
     // const myElement2 = <Car1 brand={var1} model={var2}/>
 
-    const carInfo = {
-        name : "Volvo",
-        model :"2010",
-        color :"Red",
-        type : "S90"
+    // const carInfo = {
+    //     name : "Volvo",
+    //     model :"2010",
+    //     color :"Red",
+    //     type : "S90"
+    // }
+    // const myElement3 = <Car1 brand={carInfo}/>
+
+    const person = {
+        name : "Hakan",
+        age  : "54",
+        location : "Istanbul"
     }
-    const myElement3 = <Car1 brand={carInfo}/>
+    const myCar = {
+        carName :"Nissan",
+        carAge  :10,
+        carModel   :"Quasqai"
+    }
+    const myProps = {
+        person,myCar
+    }
+
+    const myElement4 = <Car1 definition={myProps} addition={myCar}/>
+
   return (
     <div>
         {/* {myElement}
         {myElement2} */}
-        {myElement3}
+        {/* {myElement3} */}
+        {myElement4}
     </div>
   )
 }
