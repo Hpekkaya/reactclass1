fe9c61c9c7c6fe61f0051e22a82e38c6b25e5d52
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-12 12:34:35 +0300

    More than one contents with Props

--- src/2-Props/Car1.js
+++ src/2-Props/Car1.js
@@ -4,9 +4,14 @@ const Car1 = (props) => {
   return (
     <div>
         {/* <h2>My Car brand is {props.brand} and model is {props.model}</h2> */}
-        <h2>My Car :  </h2>
+        {/* <h2>My Car :  </h2>
         <h2> Brand : {props.brand.name} Model : {props.brand.model}  </h2>
-        <h2> Color : {props.brand.color} Type : {props.brand.type}  </h2>
+        <h2> Color : {props.brand.color} Type : {props.brand.type}  </h2> */}
+        
+        <h2>{props.definition.person.name}</h2>
+        <h2>{props.definition.person.age}</h2>
+        <h2>{props.definition.myCar.carName}</h2>
+        <h2>{props.definition.myCar.carAge}</h2>
     </div>
   )
 }
