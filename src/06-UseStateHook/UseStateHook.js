d5eaca64627f4eda10634b970d8aa6098c5a1946
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-13 17:17:36 +0300

    UseState tried

--- src/06-UseStateHook/UseStateHook.js
+++ src/06-UseStateHook/UseStateHook.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseStateHook = () => {
+    const [color, setColor] = useState("Brown")
+    const [counter, setCounter] = useState(0)
   return (
-    <div>UseStateHook</div>
+    <div>
+        <h2>My Favorite Color is {color} </h2>
+        <button type='button' onClick={()=>setColor("Blue")}>Change Color</button>
+
+        <h2>Counter is : {counter} </h2>
+        <button type='button' onClick={()=>setCounter(counter-1)}>Decrease</button>
+        <button type='button' onClick={()=>setCounter(counter+1)}>Increase</button>
+    </div>
   )
 }
 
