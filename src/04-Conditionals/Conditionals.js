b34d2b8af74db5f913bb37704c16a834e30d5be5
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-13 11:19:41 +0300

    Conditional 2nd phase repeated

--- src/04-Conditionals/Conditionals.js
+++ src/04-Conditionals/Conditionals.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Goal from './Goal'
+import Garage from './Garage'
 
 const Conditionals = () => {
+    const cars = ["Ford","Volvo", "Nissan", "BMW"]
   return (
     <div>
-        <Goal isGoal={true}/>
+        {/* <Goal isGoal={true}/> */}
+        <Garage cars={cars}/>
     </div>
   )
 }
--- src/04-Conditionals/Garage.js
+++ src/04-Conditionals/Garage.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Garage = () => {
+const Garage = ({cars}) => {
+    // const cars = props.cars  //(props)
   return (
-    <div>Garage</div>
+    <div>
+        <h1>Garage</h1>
+        {cars?.length >0 && <h2>In the Garage there are {cars.length} cars</h2>}
+    </div>
   )
 }
+
+// If added "?" marks, will not return error, Beceuse it means check first then continue
 
 export default Garage
