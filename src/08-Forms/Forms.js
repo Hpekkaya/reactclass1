8887c150a686ccf904de967b60a17826204893e3
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-14 10:13:54 +0300

    Select options tried

--- src/08-Forms/Forms.js
+++ src/08-Forms/Forms.js
@@ -3,11 +3,16 @@ import React, { useState } from 'react'
 const Forms = () => {
   const [name,setName]= useState()
   const [inputs,setInputs] = useState({})
+  const [myCar,setMyCar] = useState("Volvo")
+  // Single valued submit process
   // const handleSubmit =(event)=>{event.preventDefault();  alert(`You entered was : ${name}`) }
   const handleChange = (event) => {
     const name = event.target.name
     const value = event.target.value
     setInputs(values => ({...values, [name]:value}))
+  }
+  const handleChange2 = (event) => {   // For Select options in Forms in order to process value
+    setMyCar(event.target.value )   
   }
   const handleSubmit2 =(event)=> {
     event.preventDefault();  
@@ -31,6 +36,13 @@ const Forms = () => {
           onChange={handleChange} /><br></br>
         </label> <br></br>
         <button type='submit'>Submit</button>        
+      </form>
+      <form>
+        <select value={myCar} onChange={handleChange2}>
+          <option value="Ford">Ford</option>
+          <option value="Volvo">Volvo</option>
+          <option value="Nissan">Nissan</option>
+        </select>
       </form>
     </div>
   )
