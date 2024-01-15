9cc45142b752baaad1e15e4835d88b93b4bea8a4
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-15 17:27:58 +0300

    fetch user id in the contact page

--- src/09-Router/pages/Contact.js
+++ src/09-Router/pages/Contact.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Contact = () => {
+  const [users,setUsers] = useState([])
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>res.json())
+    .then((data)=>setUsers(data))
+
+  },[])
+
   return (
-    <div>Contact</div>
+    <div>
+      <h2>Contact</h2>
+      <ol>
+        {users.map((user)=><li key ={user.id}>{user.name}</li>)}
+      </ol>
+    </div>
   )
 }
 
--- src/09-Router/pages/Home.js
+++ src/09-Router/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
-    <div>Home</div>
+    <div>
+      <label>In order to contact click </label>
+        <button onClick={()=>navigate("contact")}>Contact</button><br></br>
+        <button onClick={()=>navigate(-1)}>Back</button>
+      
+    </div>
   )
 }
 
