70fab70d54ff46fea36da99032aef4b90d365876
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-15 12:36:39 +0300

    pages generated

--- src/09-Router/Router.js
+++ src/09-Router/Router.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import Blogs from './pages/Blogs'
+import Contact from './pages/Contact'
+import NoPage from './pages/NoPage'
 
-const Router = () => {
+const Router  = () => {
   return (
-    <div>Router</div>
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>}/>
+            <Route path="blogs" element={<Blogs/>}/>
+            <Route path="contact" element={<Contact/>}/>
+            <Route path="*" element={<NoPage/>}/>
+            {/* <Route path="contact/:id" element={<ContactDetail/>}/> */}
+        </Route>
+        </Routes>
+    
+    </BrowserRouter>
+    
+    
   )
 }
 
-export default Router
+export default Router;
--- src/09-Router/pages/Layout.js
+++ src/09-Router/pages/Layout.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Link, Outlet} from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <>
+        <div>Layout</div>
+        <nav>
+            <ul>
+                <li> <Link to="/">Home</Link></li>
+                <li> <Link to="/blogs">Blogs</Link></li>
+                <li> <Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+        <Outlet/>
+    </>
+    
   )
 }
 
