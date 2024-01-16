8d51ece672aa98c2ab2274d9904af48e448d2033
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-16 14:25:17 +0300

    Inline CSS applied

--- src/11-CSSStyling/CSSStyling.js
+++ src/11-CSSStyling/CSSStyling.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const CSSStyling = () => {
+    const myStyle ={color: "white", backgroundColor:"black"}
   return (
-    <div>CSSStyling</div>
+    <div>
+        <h1 style={{color:"red"}}>CSS Styling Inline in React</h1>
+        <h1 style={{color:"brown", backgroundColor:"yellow"}}>BackgroundColor Style</h1>
+        <h1 style={myStyle}> Style 1</h1>
+    </div>
   )
 }
 
