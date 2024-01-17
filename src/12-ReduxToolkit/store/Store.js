1c43cf002217889d2617e3c47013401b1485462f
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-17 23:25:49 +0300

    store structure re-reviewed

--- src/12-ReduxToolkit/store/Store.js
+++ src/12-ReduxToolkit/store/Store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../counter/CounterSlice"
 
-export const store = configureStore({
+export const store = configureStore ({
     reducer: {
         counter: counterReducer,
     }
