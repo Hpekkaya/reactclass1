6262ab8ee453ca30848677c78d95d29f52e29af8
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-17 23:57:38 +0300

    Provider re reviewed

--- src/12-ReduxToolkit/ReduxToolkit.js
+++ src/12-ReduxToolkit/ReduxToolkit.js
@@ -6,9 +6,10 @@ import Counter from './counter/Counter'
 
 const ReduxToolkit = () => {
   return (
-    <Provider store={store}>
-        <Counter/>
+    <Provider store = {store}>
+      <Counter/>
     </Provider>
+
   )
 }
 
