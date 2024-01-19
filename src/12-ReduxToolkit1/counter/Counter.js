51dfe5331753d73a7b26315b9c7b10f49c511f38
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-19 18:10:10 +0300

    Redux Toolkit re regenerated

--- src/12-ReduxToolkit1/counter/Counter.js
+++ src/12-ReduxToolkit1/counter/Counter.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './counterSlice'
 
 const Counter = () => {
+
+    // useSelector
+    // A hook to access the redux store's state. This hook takes a selector function as an argument. The selector is called with the store state.
+
+    //  This hook takes an optional equality comparison function as the second parameter that allows you to customize the way the selected state is compared to determine whether the component needs to be re-rendered.
+    const count = useSelector((state)=>state.counter.value)
+
+    const dispatch = useDispatch()
+
+    let amount = 10
+
   return (
-    <div>Counter</div>
+    <div>
+        <div style = {{fontSize :"100px"}}>{count}</div>
+        <button onClick={()=>dispatch(increment())}> Increment (+) </button>
+        <button onClick={()=>dispatch(decrement())}> Decrement (-) </button>
+        <button onClick={()=>dispatch(incrementByAmount(amount))}> Increment By {amount} </button>
+    </div>
   )
 }
 
--- src/12-ReduxToolkit1/counter/counterSlice.js
+++ src/12-ReduxToolkit1/counter/counterSlice.js
@@ -1,9 +1,27 @@
-import React from 'react'
+// Slice (R) used in Reducer
 
-const counterSlice = () => {
-  return (
-    <div>counterSlice</div>
-  )
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    value:10
 }
 
-export default counterSlice
+// Export the slice (R)
+// A function that accepts an initial state, an object full of reducer functions, 
+// And a "slice name", and automatically generates action creators 
+// And action types that correspond to the reducers and state.
+export const counterSlice = createSlice ({
+    name : "counter",
+    initialState,
+    reducers : {
+        increment : (state) => {state.value +=1} ,
+        decrement : (state) => {state.value -=1} ,
+        incrementByAmount : (state,action) => {state.value +=action.payload} ,
+        
+    }
+
+})
+
+export const {increment, decrement, incrementByAmount} = counterSlice.actions
+
+export default counterSlice.reducer
