d0468cd42d0d8750b7f222cae112f4fb47b5ff96
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-19 20:00:27 +0300

    Reducer regenerated

--- src/12-ReduxToolkit2/ReduxToolkit.js
+++ src/12-ReduxToolkit2/ReduxToolkit.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const ReduxToolkit = () => {
   return (
-    <div>ReduxToolkit</div>
+    <div>
+        <Provider store={store}>
+            Redux Toolkit1
+        </Provider>
+    </div>
   )
 }
 
